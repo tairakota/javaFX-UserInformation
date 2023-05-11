0a4f6f90882253c364ecbfba50db245e493d15a6
taira kota <taira.kota.2023.kensyu@gmail.com>
2023-05-11 22:18:15 +0900

    「例外処理」実装

--- src/main/java/com/example/javafxuserinformation/InfoController.java
+++ src/main/java/com/example/javafxuserinformation/InfoController.java
@@ -7,8 +7,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-import java.util.stream.Collectors;
-
 public class InfoController {
     @FXML private ComboBox<String> jobComboBox;
     @FXML private TextField nameTextField;
@@ -24,7 +22,7 @@ public class InfoController {
 
     ObservableList<String> items
             = FXCollections.observableArrayList("株式会社A","株式会社B","株式会社C");
-    int editId;
+    int editId = -1;
     @FXML
     private void initialize() {
         jobComboBox.setItems(items);
@@ -46,17 +44,39 @@ public class InfoController {
 
     @FXML
     public void onAdd(ActionEvent actionEvent) {
+        String job = jobComboBox.getValue();
+        String name = nameTextField.getText();
+        String scoreText = scoreTextField.getText();
+
+        if (job == null || name.trim().isEmpty() || scoreText.trim().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "すべての項目を入力してください。", ButtonType.OK);
+            alert.showAndWait();
+            return;
+        }
+
+        int score;
+        try {
+            score = Integer.parseInt(scoreText);
+        } catch (NumberFormatException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "スコアには0～100の整数を入力してください。", ButtonType.OK);
+            alert.showAndWait();
+            return;
+        }
+        if (score < 0 || score > 100) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "スコアには0～100の整数を入力してください。", ButtonType.OK);
+            alert.showAndWait();
+            return;
+        }
+
         int maxId = tableView.getItems().stream()
                 .mapToInt(User::getId)
                 .max()
                 .orElse(0);
-        String job = jobComboBox.getValue();
-        String name = nameTextField.getText();
-        int score = Integer.parseInt(scoreTextField.getText());
         User user = new User(job, name, score);
         user.setId(maxId + 1);
         tableView.getItems().add(user);
     }
+
     @FXML
     private void onDelete(ActionEvent actionEvent) {
         int selectedIndex = tableView.getSelectionModel().getSelectedIndex();
@@ -67,6 +87,11 @@ public class InfoController {
     @FXML
     private void onEdit(ActionEvent actionEvent) {
         User edituser = tableView.getSelectionModel().getSelectedItem();
+        if (edituser == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "編集するレコードを選択してください。", ButtonType.OK);
+            alert.showAndWait();
+            return;
+        }
         jobComboBoxEdit.setValue(edituser.getJob());
         nameTextFieldEdit.setText(edituser.getName());
         scoreTextFieldEdit.setText(String.valueOf(edituser.getScore()));
@@ -74,20 +99,48 @@ public class InfoController {
     }
     @FXML
     private void onUpdate(ActionEvent actionEvent) {
-        if (jobComboBoxEdit != null && nameTextFieldEdit != null && scoreTextFieldEdit != null) {
-            User selectedIndex = tableView.getSelectionModel().getSelectedItem();
-            tableView.getItems().remove(selectedIndex);
-            String job = jobComboBoxEdit.getValue();
-            String name = nameTextFieldEdit.getText();
-            int score = Integer.parseInt(scoreTextFieldEdit.getText());
-            User user = new User(job, name, score);
-            user.setId(editId);
-            tableView.getItems().add(user);
+        String job = jobComboBoxEdit.getValue();
+        String name = nameTextFieldEdit.getText();
+        String scoreText = scoreTextFieldEdit.getText();
+
+        if (editId == -1) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "編集するレコードを選択してください。", ButtonType.OK);
+            alert.showAndWait();
+            return;
         }
+
+        if (job == null || name.trim().isEmpty() || scoreText.trim().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "すべての項目を入力してください。", ButtonType.OK);
+            alert.showAndWait();
+            return;
+        }
+
+        int score;
+        try {
+            score = Integer.parseInt(scoreText);
+        } catch (NumberFormatException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "スコアには0～100の整数を入力してください。", ButtonType.OK);
+            alert.showAndWait();
+            return;
+        }
+        if (score < 0 || score > 100) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "スコアには0～100の整数を入力してください。", ButtonType.OK);
+            alert.showAndWait();
+            return;
+        }
+
+        User selectedIndex = tableView.getSelectionModel().getSelectedItem();
+        tableView.getItems().remove(selectedIndex);
+
+        User user = new User(job, name, score);
+        user.setId(editId);
+        tableView.getItems().add(user);
+
         jobComboBoxEdit.setValue(null);
         nameTextFieldEdit.setText(null);
         scoreTextFieldEdit.setText(null);
         tableView.getSortOrder().add(idColumn);
+        editId = -1;
     }
 
 
